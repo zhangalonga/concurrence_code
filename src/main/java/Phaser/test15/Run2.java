6fb9b293efca8baa45bf44521e157a9c376a5c10
zhangalong <zhangalong@yuewen.com>
2018-09-19 11:36:37 +0800

    forceTermination方法使屏障功能失效，isTerminated()判断是否已经是销毁状态

--- src/main/java/Phaser/test15/Run2.java
+++ src/main/java/Phaser/test15/Run2.java
@@ -1,7 +1,27 @@
 package Phaser.test15;
 
+import java.util.concurrent.Phaser;
+
 /**
  * created by zhangalong on 2018/9/19
  */
 public class Run2 {
+    public static void main(String[] args) {
+
+        try {
+            Phaser phaser = new Phaser(3);
+            ThreadA a = new ThreadA(phaser);
+            a.setName("A");
+            a.start();
+            ThreadB b = new ThreadB(phaser);
+            b.setName("B");
+            b.start();
+            Thread.sleep(1000);
+            phaser.forceTermination();
+            System.out.println(phaser.isTerminated());
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+    }
 }
