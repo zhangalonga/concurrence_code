0b5f17384f4d05cb6fdf65469cafb4c01c3ce1c1
zhangalong <zhangalong@yuewen.com>
2018-10-09 10:53:55 +0800

    执行三个任务，另外一个任务被拒绝执行，因为线程数已经超过max值

---
+++ src/main/java/ThreadPoolExecutor/test18/SynchronousQueueTest1.java
@@ -0,0 +1,20 @@
+package ThreadPoolExecutor.test18;
+
+import java.util.concurrent.SynchronousQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * created by zhangalong on 2018/10/9
+ */
+public class SynchronousQueueTest1 {
+    public static void main(String[] args) {
+        SynchronousQueue<Runnable> queue = new SynchronousQueue<>();
+        System.out.println(queue.size());
+        ThreadPoolExecutor executor = new ThreadPoolExecutor(2, 3, 5, TimeUnit.SECONDS, queue);
+        executor.execute(new MyRunnable());
+        executor.execute(new MyRunnable());
+        executor.execute(new MyRunnable());
+        System.out.println(executor.getPoolSize() + "笨蛋" + queue.size());
+    }
+}
