bc7601772267188ec980f9cf9c1bae8820dcd201
zhangalong <zhangalong@yuewen.com>
2018-10-10 11:08:24 +0800

    getCorePoolSize取得构造方法传入的CorePoolSize的值

---
+++ src/main/java/ThreadPoolExecutor/test22/GetCorePoolSize_Test.java
@@ -0,0 +1,15 @@
+package ThreadPoolExecutor.test22;
+
+import java.util.concurrent.SynchronousQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * created by zhangalong on 2018/10/10
+ */
+public class GetCorePoolSize_Test {
+    public static void main(String[] args) {
+        final ThreadPoolExecutor executor = new ThreadPoolExecutor(2, 5, 100, TimeUnit.SECONDS, new SynchronousQueue<>());
+        System.out.println("getCorePoolSize=" + executor.getCorePoolSize());
+    }
+}
