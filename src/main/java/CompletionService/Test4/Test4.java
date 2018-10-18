5ae8768fc3436f0fcd0e55bc8005106999f522c0
zhangalong <zhangalong@yuewen.com>
2018-10-18 14:01:13 +0800

    时间的值小于任务执行的是时间，则返回值就是null

---
+++ src/main/java/CompletionService/Test4/Test4.java
@@ -0,0 +1,22 @@
+package CompletionService.Test4;
+
+import java.util.concurrent.*;
+
+/**
+ * created by zhangalong on 2018/10/18
+ */
+public class Test4 {
+    public static void main(String[] args) throws InterruptedException, ExecutionException {
+        MyCallableA a = new MyCallableA();
+        MyCallableB b = new MyCallableB();
+        ExecutorService pool = Executors.newCachedThreadPool();
+        CompletionService service = new ExecutorCompletionService(pool);
+        service.submit(a);
+        service.submit(b);
+        System.out.println("zzzzz" + "" + service.poll(4, TimeUnit.SECONDS) + System.currentTimeMillis());
+        System.out.println("x");
+        System.out.println("zzzzz" + "" + service.poll(7, TimeUnit.SECONDS).get() + System.currentTimeMillis());
+        System.out.println("x");
+        System.out.println("main end");
+    }
+}
