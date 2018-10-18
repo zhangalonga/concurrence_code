2ac4b3cf3b240b469a93494ffde99a98287ba61d
zhangalong <zhangalong@yuewen.com>
2018-10-18 14:14:34 +0800

    任务B出现异常而任务A未输出

---
+++ src/main/java/CompletionService/Test5/Run3.java
@@ -0,0 +1,28 @@
+package CompletionService.Test5;
+
+import java.util.concurrent.*;
+
+/**
+ * created by zhangalong on 2018/10/18
+ */
+public class Run3 {
+    public static void main(String[] args) {
+        try {
+            MyCallableA a = new MyCallableA();
+            MyCallableB b = new MyCallableB();
+            ExecutorService pool = Executors.newSingleThreadExecutor();
+            CompletionService service = new ExecutorCompletionService(pool);
+            service.submit(b);
+            service.submit(a);
+            for (int i = 0; i < 2; i++) {
+                System.out.println("zzzzz" + " " + service.take().get());
+            }
+            System.out.println("main end!");
+
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+    }
+}
