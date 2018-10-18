2a2d4faa3e230aefb771526e82c7b7618c994b48
zhangalong <zhangalong@yuewen.com>
2018-10-18 14:19:36 +0800

    打印任务A返回值而任务B出现异常

---
+++ src/main/java/CompletionService/Test5/Run5.java
@@ -0,0 +1,24 @@
+package CompletionService.Test5;
+
+import java.util.concurrent.*;
+
+/**
+ * created by zhangalong on 2018/10/18
+ */
+public class Run5 {
+    public static void main(String[] args) throws InterruptedException, ExecutionException {
+        MyCallableA a = new MyCallableA();
+        MyCallableB b = new MyCallableB();
+        ExecutorService pool = Executors.newSingleThreadExecutor();
+        CompletionService service = new ExecutorCompletionService(pool);
+        service.submit(a);
+        service.submit(b);
+        for (int i = 0; i < 2; i++) {
+            System.out.println("zzzzz" + " " + service.poll());
+        }
+        Thread.sleep(6000);
+        System.out.println("A处" + "" + service.poll().get());
+        System.out.println("B处" + "" + service.poll().get());
+        System.out.println("main end");
+    }
+}
