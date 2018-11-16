1f96a0db8e5ff0a164ed809ae970b355b3ea280c
zhangalong <zhangalong@yuewen.com>
2018-11-16 14:36:43 +0800

    队列中无任务的原因是任务马上被执行了，并未放入队列中，所以队列的size为0，说明可以结合if判断，可以任务结束

---
+++ src/main/java/scheduledExecutorService/test9/Run2.java
@@ -0,0 +1,24 @@
+package scheduledExecutorService.test9;
+
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.ScheduledThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * created by zhangalong on 2018/11/16
+ */
+public class Run2 {
+    public static void main(String[] args) throws InterruptedException {
+        ScheduledThreadPoolExecutor executor = new ScheduledThreadPoolExecutor(10);
+        final MyRunnable runnable = new MyRunnable("A");
+        final ScheduledFuture<?> scheduledFuture = executor.schedule(runnable, 0, TimeUnit.SECONDS);
+        Thread.sleep(2000);
+        final BlockingQueue<Runnable> queue = executor.getQueue();
+        System.out.println("A处size=" + queue.size());
+        System.out.println("取消结果为：" + scheduledFuture.cancel(true));
+        final BlockingQueue<Runnable> queue1 = executor.getQueue();
+        System.out.println("B处size=" + queue1.size());
+        System.out.println("main end");
+    }
+}
