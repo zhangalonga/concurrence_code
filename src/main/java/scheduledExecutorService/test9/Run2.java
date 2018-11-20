d3e1e426b034738c6103061f8bced34da0749630
zhangalong <zhangalong@yuewen.com>
2018-11-20 13:33:35 +0800

    setRemoveOnCancelPolicy(true)结合cancel(true),则队列中的任务被删除

--- src/main/java/scheduledExecutorService/test9/Run2.java
+++ src/main/java/scheduledExecutorService/test9/Run2.java
@@ -1,5 +1,6 @@
 package scheduledExecutorService.test9;
 
+import java.util.Iterator;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.ScheduledFuture;
 import java.util.concurrent.ScheduledThreadPoolExecutor;
@@ -9,16 +10,19 @@ import java.util.concurrent.TimeUnit;
  * created by zhangalong on 2018/11/16
  */
 public class Run2 {
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) {
         ScheduledThreadPoolExecutor executor = new ScheduledThreadPoolExecutor(10);
         final MyRunnable runnable = new MyRunnable("A");
-        final ScheduledFuture<?> scheduledFuture = executor.schedule(runnable, 0, TimeUnit.SECONDS);
-        Thread.sleep(2000);
+        final ScheduledFuture<?> scheduledFuture = executor.schedule(runnable, 1, TimeUnit.SECONDS);
+        executor.setRemoveOnCancelPolicy(true);
+        System.out.println(scheduledFuture.cancel(true));
+        System.out.println(" ");
         final BlockingQueue<Runnable> queue = executor.getQueue();
-        System.out.println("A处size=" + queue.size());
-        System.out.println("取消结果为：" + scheduledFuture.cancel(true));
-        final BlockingQueue<Runnable> queue1 = executor.getQueue();
-        System.out.println("B处size=" + queue1.size());
+        final Iterator<Runnable> iterator = queue.iterator();
+        while (iterator.hasNext()) {
+            Runnable runnable1 = iterator.next();
+            System.out.println("队列中的：" + runnable1);
+        }
         System.out.println("main end");
     }
 }
