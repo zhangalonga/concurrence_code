158adbaa04dc6c37db13ff8dc9f9fe78041205bd
zhangalong <zhangalong@yuewen.com>
2018-11-22 13:44:24 +0800

    调用shutdown()方法不具有中断的效果

---
+++ src/main/java/forkJoinPool/test16/Test4.java
@@ -0,0 +1,21 @@
+package forkJoinPool.test16;
+
+import java.util.concurrent.ForkJoinPool;
+
+/**
+ * created by zhangalong on 2018/11/22
+ */
+public class Test4 {
+    public static void main(String[] args) throws InterruptedException {
+        final MyRunnable runnable = new MyRunnable();
+        ForkJoinPool pool = new ForkJoinPool();
+        pool.submit(runnable);
+        Thread.sleep(1000);
+        pool.shutdown();
+        if (pool.isShutdown() == false) {
+            pool.submit(runnable);
+        }
+        System.out.println("main end");
+        Thread.sleep(Integer.MAX_VALUE);
+    }
+}
