b1bc9c77fa4510d33979ea8cc8fcdcc138b16986
zhangalong <zhangalong@yuewen.com>
2018-11-22 14:09:58 +0800

    shutdownNow()只能维持一个瞬间

---
+++ src/main/java/forkJoinPool/test18/Run3.java
@@ -0,0 +1,35 @@
+package forkJoinPool.test18;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.ForkJoinTask;
+
+/**
+ * created by zhangalong on 2018/11/22
+ */
+public class Run3 {
+    public static void main(String[] args) throws InterruptedException, ExecutionException {
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < Integer.MAX_VALUE / 100; i++) {
+                    String s = new String();
+                    Math.random();
+                    Math.random();
+                    Math.random();
+                    Math.random();
+                    Math.random();
+                    Math.random();
+                }
+            }
+        };
+        ForkJoinPool pool = new ForkJoinPool();
+        final ForkJoinTask<?> task = pool.submit(runnable);
+        Thread.sleep(500);
+        System.out.println("A=" + pool.isTerminating());
+        pool.shutdownNow();
+        System.out.println("B=" + pool.isTerminating());
+        Thread.sleep(1000);
+        System.out.println("C=" + pool.isTerminated());
+    }
+}
