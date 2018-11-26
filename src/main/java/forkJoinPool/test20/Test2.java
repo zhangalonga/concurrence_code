8c46d4ac16c4d557272a2196a332c0335880e8d5
zhangalong <zhangalong@yuewen.com>
2018-11-26 13:39:34 +0800

    awaitTermination方法要结合shutdown方法使用

---
+++ src/main/java/forkJoinPool/test20/Test2.java
@@ -0,0 +1,21 @@
+package forkJoinPool.test20;
+
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * created by zhangalong on 2018/11/26
+ */
+public class Test2 {
+    public static void main(String[] args) throws InterruptedException {
+        final MyRunnable runnable = new MyRunnable();
+        ForkJoinPool pool = new ForkJoinPool();
+        pool.execute(runnable);
+        pool.shutdown();
+        System.out.println("main begin" + System.currentTimeMillis());
+        System.out.println(pool.awaitTermination(10, TimeUnit.SECONDS));
+        System.out.println("main end " + System.currentTimeMillis());
+
+
+    }
+}
